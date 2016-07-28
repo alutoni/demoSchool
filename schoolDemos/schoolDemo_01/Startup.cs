10e6ae15d2b553ca94ad50de7b5c5db55fd6321d
alutoni <alutonisales@gmail.com>
2016-07-28 12:54:30 +0800

    test

---
+++ schoolDemos/schoolDemo_01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(schoolDemo_01.Startup))]
+namespace schoolDemo_01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
